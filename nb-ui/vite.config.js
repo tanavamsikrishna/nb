e22ad70d920d742cdf17c5e9adeeeac2e82e96f0
Vamsi Talupula <tanavamsikrishna@outlook.com>
2026-06-14 23:28:59 +0530

    feat: add DuckDB-WASM dependency with Vite config

--- nb-ui/vite.config.js
+++ nb-ui/vite.config.js
@@ -4,6 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  optimizeDeps: {
+    exclude: ['@duckdb/duckdb-wasm']
+  },
   server: {
     proxy: {
       '/stream': 'http://localhost:7777'
